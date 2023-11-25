f5f6c3e5cbabd25476d59516d51d9cf77c04c475
ddowfs <dev11fs201@gmail.com>
2023-11-25 10:27:11 +0100

    bootstrap

--- git-react/src/App.jsx
+++ git-react/src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-        <h1>Heelo World</h1>
+        <h1 className='text-primary'>Heelo World</h1>
     </>
   )
 }
